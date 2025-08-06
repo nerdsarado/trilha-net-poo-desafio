49bd71d358f15ee31f45ecea33d8108a3f5ed970
nerdsarado <lucascostaborges2007@gmail.com>
2025-08-06 00:52:00 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private int Modelo;
+        private int Imei;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, int modelo, int imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
